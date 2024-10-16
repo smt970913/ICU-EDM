95940dea0cf796469003fb1c383f65e134725b15
Maotong SUN <maotong.sun@tum.de>
2024-10-16 15:41:03 +0200

    general modification

--- models/DataMIMIC_IV_EFR.py
+++ models/DataMIMIC_IV_EFR.py
@@ -18,7 +18,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 class DataLoader:
-    def __init__(self, cfg, state_id_path, rl_state_path, test_size = 0.20, random_state = 1024, scaler = StandardScaler()):
+    def __init__(self, cfg, state_id_path, rl_state_path, test_size = 0.20, random_state = 68, scaler = StandardScaler()):
         self.cfg = cfg
         self.state_id_path = state_id_path
         self.rl_state_path = rl_state_path
@@ -89,28 +89,19 @@ class DataLoader:
         self.rl_state_var_test = self.rl_state_var.loc[self.test_index_list].copy()
 
         self.train_state_table_cont = self.state_df_id_cont[self.state_df_id_cont['ext_id'].isin(self.train_ext_id.tolist())].copy()
-        self.test_state_table_cont = self.state_df_id_cont[self.state_df_id_cont['ext_id'].isin(self.test_ext_id.tolist())].copy()
 
         self.train_state_table_ext = self.state_df_id_ext[self.state_df_id_ext['ext_id'].isin(self.train_ext_id.tolist())].copy()
-        self.test_state_table_ext = self.state_df_id_ext[self.state_df_id_ext['ext_id'].isin(self.test_ext_id.tolist())].copy()
 
         self.train_index_list_cont = self.train_state_table_cont.index.tolist()
         self.train_index_list_ext = self.train_state_table_ext.index.tolist()
 
-        self.test_index_list_cont = self.test_state_table_cont.index.tolist()
-        self.test_index_list_ext = self.test_state_table_ext.index.tolist()
-
         self.rl_state_var_sc_train_cont = self.rl_state_var_sc.loc[self.train_index_list_cont].copy()
-        self.rl_state_var_sc_test_cont = self.rl_state_var_sc.loc[self.test_index_list_cont].copy()
 
         self.rl_state_var_sc_train_ext = self.rl_state_var_sc.loc[self.train_index_list_ext].copy()
-        self.rl_state_var_sc_test_ext = self.rl_state_var_sc.loc[self.test_index_list_ext].copy()
 
         self.rl_state_var_train_cont = self.rl_state_var.loc[self.train_index_list_cont].copy()
-        self.rl_state_var_test_cont = self.rl_state_var.loc[self.test_index_list_cont].copy()
 
         self.rl_state_var_train_ext = self.rl_state_var.loc[self.train_index_list_ext].copy()
-        self.rl_state_var_test_ext = self.rl_state_var.loc[self.test_index_list_ext].copy()
 
         self.terminal_state = np.zeros(self.rl_state_var_sc.shape[1])
 
@@ -142,31 +133,23 @@ class DataLoader:
             self.train_memory_ext.push(state, action, obj_cost, con_cost, next_state, done)
 
     def data_buffer_test(self):
-        self.test_memory_cont = OCRL_model_fqi.ReplayBuffer(self.cfg.memory_capacity)
-        self.test_memory_ext = OCRL_model_fqi.ReplayBuffer(self.cfg.memory_capacity)
+        self.test_memory = OCRL_model_fqi.ReplayBuffer(self.cfg.memory_capacity)
 
-        for i in range(len(self.test_state_table_cont)):
-            state = self.rl_state_var_sc_test_cont.values[i]
-            action = self.test_state_table_cont['EXT'].values[i]
-            obj_cost = self.test_state_table_cont['obj_costs'].values[i]
-            con_cost = self.test_state_table_cont['con_costs'].values[i]
-            done = self.test_state_table_cont['EXT'].values[i]
+        for i in range(len(self.test_state_table)):
 
-            idx = self.test_index_list_cont[i]
-            next_state = self.rl_state_var_sc.loc[idx + 1].values
+            state = self.rl_state_var_sc_test.values[i]
+            action = self.test_state_table['EXT'].values[i]
+            obj_cost = self.test_state_table['obj_costs'].values[i]
+            con_cost = self.test_state_table['con_costs'].values[i]
+            done = self.test_state_table['EXT'].values[i]
 
-            self.test_memory_cont.push(state, action, obj_cost, con_cost, next_state, done)
+            if action == 1:
+                next_state = self.terminal_state.copy()
+            else:
+                idx = self.test_index_list_cont[i]
+                next_state = self.rl_state_var_sc.loc[idx + 1].values
 
-        for i in range(len(self.test_state_table_ext)):
-            state = self.rl_state_var_sc_test_ext.values[i]
-
-            action = self.test_state_table_ext['EXT'].values[i]
-            obj_cost = self.test_state_table_ext['obj_costs'].values[i]
-            con_cost = self.test_state_table_ext['con_costs'].values[i]
-            done = self.test_state_table_ext['EXT'].values[i]
-            next_state = self.terminal_state.copy()
-
-            self.test_memory_ext.push(state, action, obj_cost, con_cost, next_state, done)
+            self.test_memory.push(state, action, obj_cost, con_cost, next_state, done)
 
     def data_torch_loader_train(self):
         state_batch_1, action_batch_1, obj_cost_batch_1, con_cost_batch_1, next_state_batch_1, done_batch_1 = self.train_memory_cont.sample(self.cfg.batch_size_cont)
@@ -198,21 +181,13 @@ class DataLoader:
         return state_batch, action_batch, obj_cost_batch, con_cost_batch_dict, next_state_batch, done_batch
 
     def data_torch_loader_test(self):
-        state_batch_1, action_batch_1, obj_cost_batch_1, con_cost_batch_1, next_state_batch_1, done_batch_1 = self.test_memory_cont.sample(self.cfg.batch_size_cont)
-        state_batch_2, action_batch_2, obj_cost_batch_2, con_cost_batch_2, next_state_batch_2, done_batch_2 = self.test_memory_ext.sample(self.cfg.batch_size_ext)
+        state_batch, action_batch, obj_cost_batch, con_cost_batch, next_state_batch, done_batch = self.test_memory.extract()
 
-        constraint_num = len(con_cost_batch_1)
-
-        state_batch = state_batch_1 + state_batch_2
-        action_batch = action_batch_1 + action_batch_2
-        obj_cost_batch = obj_cost_batch_1 + obj_cost_batch_2
+        constraint_num = len(con_cost_batch)
 
         con_cost_batch_dict = {i: [] for i in range(constraint_num)}
         for m in range(constraint_num):
-            con_cost_batch_dict[m] = tuple(con_cost_batch_1[m]) + tuple(con_cost_batch_2[m])
-
-        next_state_batch = next_state_batch_1 + next_state_batch_2
-        done_batch = done_batch_1 + done_batch_2
+            con_cost_batch_dict[m] = tuple(con_cost_batch[m])
 
         state_batch = torch.tensor(np.array(state_batch), device = self.cfg.device, dtype = torch.float)
         action_batch = torch.tensor(np.array(action_batch), device = self.cfg.device, dtype = torch.long).unsqueeze(1)
--- models/test_set.py
+++ models/test_set.py
@@ -1,92 +1,22 @@
 import sys
 import os
 
-import torch
-import numpy as np
-from tqdm import tqdm
-
 script_dir = os.path.dirname(__file__)
 sys.path.append(script_dir)
-
-import OCRL_model_fqi
 
 import warnings
 warnings.filterwarnings('ignore')
 
 class RLTesting:
-    def __init__(self, cfg, state_dim, action_dim, hidden_layers, test_agent, data_loader):
+    def __init__(self, cfg, fqe_agent, data_loader):
         self.cfg = cfg
-        self.state_dim = state_dim
-        self.action_dim = action_dim
-        self.hidden_layers = hidden_layers
-        self.test_agent = test_agent
+        self.fqe_agent = fqe_agent
         self.data_loader = data_loader
 
-    def fqe_agent_config(self, eval_target, seed = 100):
-        agent_fqe_test = OCRL_model_fqi.FQE(self.cfg, self.state_dim, self.action_dim, self.hidden_layers, self.test_agent, eval_target)
-        torch.manual_seed(seed)
-        return agent_fqe_test
+    def test(self):
 
-    def test(self, agent_fqe_obj, agent_fqe_con_list):
-        print('Start to test!')
-        print(f'Algorithm:{self.cfg.algo}, Device:{self.cfg.device}')
+        state_batch, action_batch, obj_cost_batch, con_cost_batch_dict, next_state_batch, done_batch = self.data_loader()
 
-        self.loss_eva_obj = []
-        self.FQE_est_value_obj = []
+        test_eval_cost = self.fqe_agent.avg_Q_value_est(state_batch)
 
-        self.loss_eva_mv_obj = []
-        self.FQE_est_value_mv_obj = []
-        self.FQE_est_ci_lb_mv_obj = []
-        self.FQE_est_ci_ub_mv_obj = []
-
-        self.loss_eva_con = {i: [] for i in range(len(agent_fqe_con_list))}
-        self.FQE_est_value_con = {i: [] for i in range(len(agent_fqe_con_list))}
-
-        self.loss_eva_mv_con = {i: [] for i in range(len(agent_fqe_con_list))}
-        self.FQE_est_value_mv_con = {i: [] for i in range(len(agent_fqe_con_list))}
-        self.FQE_est_ci_lb_mv_con = {i: [] for i in range(len(agent_fqe_con_list))}
-        self.FQE_est_ci_ub_mv_con = {i: [] for i in range(len(agent_fqe_con_list))}
-
-        for k in tqdm(range(self.cfg.test_eps)):
-            state_batch, action_batch, obj_cost_batch, con_cost_batch_dict, next_state_batch, done_batch = self.data_loader()
-
-            # update the policy agent for test evaluation agent (FQE)
-            loss_ev_obj = agent_fqe_obj.update(state_batch, action_batch, obj_cost_batch, next_state_batch, done_batch)
-            self.loss_eva_obj.append(loss_ev_obj)
-
-            for m in range(len(agent_fqe_con_list)):
-                self.loss_eva_con[m].append(agent_fqe_con_list[m].update(state_batch, action_batch, con_cost_batch_dict[m], next_state_batch, done_batch))
-
-            fqe_obj_est_value = agent_fqe_obj.avg_Q_value_est(state_batch)
-            self.FQE_est_value_obj.append(fqe_obj_est_value)
-
-            for m in range(len(agent_fqe_con_list)):
-                self.FQE_est_value_con[m].append(agent_fqe_con_list[m].avg_Q_value_est(state_batch))
-            ######################################################################################
-            if k % self.cfg.target_update == 0:
-                ### update the target agent for test agent (FQE objective cost)
-                for target_param, policy_param in zip(agent_fqe_obj.target_net.parameters(), agent_fqe_obj.policy_net.parameters()):
-                    target_param.data.copy_(self.cfg.tau * policy_param.data + (1 - self.cfg.tau) * target_param.data)
-                ### update the target agent for test agent (FQE constraint cost)
-                for agent_fqe_con in agent_fqe_con_list:
-                    for target_param, policy_param in zip(agent_fqe_con.target_net.parameters(), agent_fqe_con.policy_net.parameters()):
-                        target_param.data.copy_(self.cfg.tau * policy_param.data + (1 - self.cfg.tau) * target_param.data)
-
-            if k % 1000 == 0:
-                # print(f'Episode:{k}, Loss:{np.mean(self.loss_eva_obj)}, FQE_est_value_obj:{np.mean(self.FQE_est_value_obj)}')
-                self.loss_eva_mv_obj.append(np.mean(self.loss_eva_obj))
-                self.FQE_est_value_mv_obj.append(np.mean(self.FQE_est_value_obj))
-                self.FQE_est_ci_lb_mv_obj.append(np.percentile(self.FQE_est_value_obj, 2.5))
-                self.FQE_est_ci_ub_mv_obj.append(np.percentile(self.FQE_est_value_obj, 97.5))
-
-                for m in range(len(agent_fqe_con_list)):
-                    # print(f'Episode:{k}, Loss:{np.mean(self.loss_eva_con[m])}, FQE_est_value_con:{np.mean(self.FQE_est_value_con[m])}')
-                    self.loss_eva_mv_con[m].append(np.mean(self.loss_eva_con[m]))
-                    self.FQE_est_value_mv_con[m].append(np.mean(self.FQE_est_value_con[m]))
-                    self.FQE_est_ci_lb_mv_con[m].append(np.percentile(self.FQE_est_value_con[m], 2.5))
-                    self.FQE_est_ci_ub_mv_con[m].append(np.percentile(self.FQE_est_value_con[m], 97.5))
-
-        print("Complete Testing!")
-
-        return self.loss_eva_obj, self.FQE_est_value_obj, self.loss_eva_mv_obj, self.FQE_est_value_mv_obj, self.FQE_est_ci_lb_mv_obj, self.FQE_est_ci_ub_mv_obj, self.loss_eva_con, self.FQE_est_value_con, self.loss_eva_mv_con, self.FQE_est_value_mv_con, self.FQE_est_ci_lb_mv_con, self.FQE_est_ci_ub_mv_con
-
+        return test_eval_cost
